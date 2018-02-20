9b51b73fef3cabedc8e2c4ce57617babfdf0fc79
iregon <0iregon0@gmail.com>
2018-02-20 13:14:39 +0100

    query studenti

--- query.sql
+++ query.sql
@@ -1,3 +1,26 @@
 INSERT INTO studenti (`Cognome`, `Nome`, `DataNascita`, `CodiceClasse`) VALUES
 ("Tornesello", "Alessandro", "1999-07-19", "5"),
 ("D'Aquino", "Marco", "1999-05-26", "5");
+
+INSERT INTO studenti (`Cognome`, `Nome`, `DataNascita`, `CodiceClasse`) VALUES
+("Russo", "Mario", "1999-08-03", "5"),
+("Muho", "Manuel", "1999-05-12", "5"),
+("Loris", "Parata", "1999-09-21", "5"),
+("Coronese", "Samuele", "1999-02-05", "5"),
+("Lisi", "Simone", "1999-11-17", "5"),
+("Branca", "Lorenzo", "1999-06-03", "5"),
+("Leo", "Alberto", "1999-02-15", "5"),
+("Antonazzo", "Sebastiano Rocco", "1999-10-28", "5");
+
+INSERT INTO studenti (`Cognome`, `Nome`, `DataNascita`, `CodiceClasse`) VALUES
+("Milano", "Gabriele", "1999-11-05", "6"),
+("Longo", "Lorenzo", "1999-08-09", "21"),
+("Spiri", "Nicolas", "1999-05-03", "12"),
+("Giulia", "Fortunato", "1999-04-07", "4"),
+("Rocca", "Francesco", "1999-08-03", "7"),
+("Leopizzi", "Matteo", "1999-12-08", "9"),
+("Pischietto", "Antonio", "1999-10-05", "31"),
+("Scorrano", "Luigi", "1999-06-09", "25"),
+("Mazzeo", "Antonio", "1999-04-05", "17"),
+("Primiceri", "Edoardo", "1999-05-09", "19");
+
